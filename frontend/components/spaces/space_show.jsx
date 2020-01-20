a0112e04f7ee12e078bfb55858d5364944f3899e
Stanley <stanley.traub@gmail.com>
2020-01-20 14:51:46 -0800

    Working on space show

--- frontend/components/spaces/space_show.jsx
+++ frontend/components/spaces/space_show.jsx
@@ -19,11 +19,21 @@ class SpaceShow extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="space-show-main-div">
                 <div className="space-pics">
                     <img className="space-show-main-pic" src={this.state.space.main_pic}></img>
                 </div>
+                <div className="space-show-info">
+                    <div className='space-summary'>
+                        <span className="space-show-name">{this.state.space.name}</span>
+                        <div>{this.state.space.city}</div>
+                    </div>
+                    <div>
+                        <p>{this.state.space.description}</p>
+                    </div>
+                </div>
             </div>
+        
         )
     }
 }
