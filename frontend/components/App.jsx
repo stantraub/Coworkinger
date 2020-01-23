bb1d4be9b0f07967ce96a1c398af505bcbbfa553
Stanley <stanley.traub@gmail.com>
2020-01-23 10:43:31 -0800

    Changed app name

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route} from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 
 // import Modal from "./modals/session_modal";
