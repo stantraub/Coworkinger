ee1f1842cc151ce9e0fad0e9f1304ebb11f1f62c
Stanley <stanley.traub@gmail.com>
2020-02-01 08:14:19 -0800

    More cleanup for space show

--- frontend/components/modals/pics_carousel.jsx
+++ frontend/components/modals/pics_carousel.jsx
@@ -3,28 +3,17 @@ import React, { Component } from 'react'
 export default class PicsCarousel extends Component {
     constructor(props) {
         super(props)
-        this.state ={
+        
+        this.state = {
             currentIdx: 0
         }
     }
 
-    componentDidMount() {
-        console.log(this.props)
-    }
-
     changePic(length, change) {
-        if (change === -1) {
-            if (this.state.currentIdx === 0) {
+        if (this.state.currentIdx === 0 && change === -1) {
                 this.setState({currentIdx: length -1})
-            } else {
-                this.setState({currentIdx: this.state.currentIdx - 1})
-            }
         } else {
-            if (this.state.currentIdx === length - 1) {
-                this.setState({currentIdx: 0})
-            } else {
-                this.setState({currentIdx: this.state.currentIdx + 1})
-            }
+            this.setState({currentIdx: (this.state.currentIdx + change) % length})
         }
     }
 
@@ -32,24 +21,22 @@ export default class PicsCarousel extends Component {
         const { currentIdx } = this.state
         const { spacePics } = this.props
         return (
-                <div className="carousel-child">
-                    <div className="carousel-main-pic-wrapper">
-                        <img onClick={() => this.changePic(spacePics.length, -1)} className="carousel-arrows" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronLeft-512.png"></img>
-                        <img className="carousel-main-pic" src={spacePics[currentIdx]}></img>
-                        <img onClick={() => this.changePic(spacePics.length, 1)} className="carousel-arrows" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronRight-512.png"></img>
-
+            <div className="carousel-child">
+                <div className="carousel-main-pic-wrapper">
+                    <img onClick={() => this.changePic(spacePics.length, -1)} className="carousel-arrows" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronLeft-512.png"></img>
+                    <img className="carousel-main-pic" src={spacePics[currentIdx]}></img>
+                    <img onClick={() => this.changePic(spacePics.length, 1)} className="carousel-arrows" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronRight-512.png"></img>
+                </div>
+                <div className="carousel-sidebar">
+                    <div onClick={() => this.props.handleClick()} className="carousel-x-button">
+                        <img className="carousel-x-img" src="http://cdn.onlinewebfonts.com/svg/img_170267.png"></img>
                     </div>
-                    <div className="carousel-sidebar">
-                        <div onClick={() => this.props.handleClick()} className="carousel-x-button">
-                            <img className="carousel-x-img" src="http://cdn.onlinewebfonts.com/svg/img_170267.png"></img>
-                        </div>
-                        <div className="carousel-pic-count">
-                            {currentIdx + 1} / {spacePics.length}
-                        </div>
+                    <div className="carousel-pic-count">
+                        {currentIdx + 1} / {spacePics.length}
                     </div>
                 </div>
+            </div>
         )
-       
     }
 }
 
