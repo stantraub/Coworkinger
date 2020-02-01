ce5a9718eac3786b7e0ab09da3b9208311eb66c9
Stanley <stanley.traub@gmail.com>
2020-01-31 19:11:08 -0800

    Started image carousel

--- frontend/components/modals/pics_carousel.jsx
+++ frontend/components/modals/pics_carousel.jsx
@@ -10,17 +10,27 @@ export default class PicsCarousel extends Component {
         this.props.fetchSpace(this.props.match.params.id)
     }
 
+    currentPic(spacePics, idx) {
+        return spacePics[idx]
+    }
+
     render() {
         const { space = {}} = this.props
         if (space.space_pics) {
             return (
                 <div className="carousel-background">
                     <div className="carousel-child">
-                        <img className="carousel-main-pic" src={space.space_pics[0]}></img>
+                        <div className="carousel-main-pic-wrapper">
+                            <img className="carousel-arrows" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronLeft-512.png"></img>
+                            <img className="carousel-main-pic" src={this.currentPic(space.space_pics, 0)}></img>
+                            <img className="carousel-arrows" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronRight-512.png"></img>
+
+                        </div>
                         <div className="carousel-sidebar">
                             <Link to={`/spaces/${space.id}`} className="carousel-x-button">
                                 <img className="carousel-x-img" src="http://cdn.onlinewebfonts.com/svg/img_170267.png"></img>
                             </Link>
+                            <div></div>
                         </div>
                     </div>
                 </div>
