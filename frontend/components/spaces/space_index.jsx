de74c794290530b319d68c6073689d2a37474d8a
Stanley <stanley.traub@gmail.com>
2020-02-01 22:10:36 -0800

    Mobile view for space index

--- frontend/components/spaces/space_index.jsx
+++ frontend/components/spaces/space_index.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import SpaceItem from './space_item';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
 
 class Spaces extends React.Component {
     constructor(props) {
@@ -12,41 +18,76 @@ class Spaces extends React.Component {
     }   
 
     render() {
-        if (this.props.spaces.length === 0) {
-            return (
-                <div>
-                    There are no spaces in this location
-                </div>
-            )
-        } else {
+        if (isBrowser) {
+            if (this.props.spaces.length === 0) {
+            return <div>There are no spaces in this location</div>;
+            } else {
             return (
                 <div className="space-index-main">
-                    <h1 className="space-index-header">All Coworking Spaces in San Francisco</h1>
-                    <div className="spaces-index-wrapper">
-                        {this.props.spaces.map(space => (
-                            <SpaceItem 
-                                key={space.id}
-                                spaceId={space.id}
-                                neighborhood={space.neighborhood}
-                                peopleCapacity={space.people_capacity}
-                                officeCapacity={space.office_capacity}
-                                availability={space.availability}
-                                deskDay={space.desk_day}
-                                name={space.name}
-                                address={space.address}
-                                city={space.city}
-                                state={space.state}
-                                cost={space.cost}
-                                mainPic={space.main_pic}
-                                openHour={space.open_hour}
-                                closingHour={space.closing_hour}
-                                email={space.email}
-                                phone={space.phone}
-                            />
-                        ))}
+                <h1 className="space-index-header">
+                    All San Francisco workspaces
+                </h1>
+                <div className="spaces-index-wrapper">
+                    {this.props.spaces.map(space => (
+                    <SpaceItem
+                        key={space.id}
+                        spaceId={space.id}
+                        neighborhood={space.neighborhood}
+                        peopleCapacity={space.people_capacity}
+                        officeCapacity={space.office_capacity}
+                        availability={space.availability}
+                        deskDay={space.desk_day}
+                        name={space.name}
+                        address={space.address}
+                        city={space.city}
+                        state={space.state}
+                        cost={space.cost}
+                        mainPic={space.main_pic}
+                        openHour={space.open_hour}
+                        closingHour={space.closing_hour}
+                        email={space.email}
+                        phone={space.phone}
+                    />
+                    ))}
                     </div>
                 </div>
-            )
+                );
+            }
+        } else {
+              if (this.props.spaces.length === 0) {
+                return <div>There are no spaces in this location</div>;
+              } else {
+                return (
+                  <div className="space-index-main-mobile">
+                    <h1 className="space-index-header-mobile">
+                      All San Francisco workspaces
+                    </h1>
+                    <div className="spaces-index-wrapper">
+                      {this.props.spaces.map(space => (
+                        <SpaceItem
+                          key={space.id}
+                          spaceId={space.id}
+                          neighborhood={space.neighborhood}
+                          peopleCapacity={space.people_capacity}
+                          officeCapacity={space.office_capacity}
+                          availability={space.availability}
+                          deskDay={space.desk_day}
+                          name={space.name}
+                          address={space.address}
+                          city={space.city}
+                          state={space.state}
+                          cost={space.cost}
+                          mainPic={space.main_pic}
+                          openHour={space.open_hour}
+                          closingHour={space.closing_hour}
+                          email={space.email}
+                          phone={space.phone}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                );
+            }
         }
     }
 }
--- frontend/components/spaces/space_item.jsx
+++ frontend/components/spaces/space_item.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
 
 class SpaceItem extends React.Component {
     constructor(props) {
@@ -34,6 +40,7 @@ class SpaceItem extends React.Component {
 
     }
     render() {
+      if (isBrowser) {
         return (
           <div className="space-item-wrapper">
             <Link to={`/spaces/${this.props.spaceId}`} className="space-link">
@@ -43,19 +50,52 @@ class SpaceItem extends React.Component {
               <div className="space-item-info">
                 <div className="space-item-name">{this.props.name}</div>
                 <div className="space-item-amenities">
-                  <div>{this.props.city}, {this.props.state}</div>
+                  <div>
+                    {this.props.city}, {this.props.state}
+                  </div>
                   <span>{this.props.neighborhood}</span>
-                  <div className="space-amenity-wrapper">{this.includedAmenities()}</div>
+                  <div className="space-amenity-wrapper">
+                    {this.includedAmenities()}
+                  </div>
                   <div className="space-item-contact">
                     <div>{this.phone()}</div>
                     <div>{this.email()}</div>
                   </div>
                 </div>
-                <div className="space-item-cost"><strong>${this.props.cost}</strong> per desk / month</div>
+                <div className="space-item-cost">
+                  <strong>${this.props.cost}</strong> per desk / month
+                </div>
               </div>
             </Link>
           </div>
         );
+      } else {
+        return (
+          <div className="space-item-wrapper">
+            <Link to={`/spaces/${this.props.spaceId}`} className="space-link">
+              <img className="main-pic-mobile" src={this.props.mainPic} />
+            </Link>
+            <Link to={`/spaces/${this.props.spaceId}`} className="space-link">
+              <div>
+                <div className="space-item-name-mobile">{this.props.name}</div>
+                <div className="space-item-amenities">
+                  <div>
+                    {this.props.city}, {this.props.state}
+                  </div>
+                  <span>{this.props.neighborhood}</span>
+                  <div className="space-amenity-wrapper">
+                    {this.includedAmenities()}
+                  </div>
+                </div>
+                <div className="space-item-cost-mobile">
+                  <span>${this.props.cost} per desk / month</span>
+                </div>
+              </div>
+            </Link>
+          </div>
+        );
+      }
+
     }
 }
 
