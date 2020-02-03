a363d67943e803908eae8bc6d3635042f2bf3a4e
Stanley <stanley.traub@gmail.com>
2020-02-03 08:58:06 -0800

    Mobile show page

--- frontend/components/spaces/space_show.jsx
+++ frontend/components/spaces/space_show.jsx
@@ -30,10 +30,8 @@ class SpaceShow extends React.Component {
     
 
     includedAmenities(spaceAmenities) {
-      if (spaceAmenities) {
-
+      if (isBrowser) {
         let amenities = Object.entries(spaceAmenities).slice(0, 6)
-        
         return amenities.map((amenity, i) => {
           if (amenity[0] === 'hours_24_access') {
             return <div className="amenities-item" key={i}>24/7 Access</div>
@@ -59,9 +57,75 @@ class SpaceShow extends React.Component {
             return <div className="amenities-item" key={i}>{(amenity[0][0].toUpperCase() + amenity[0].slice(1)).split("_").join(" ")}</div>
           }
         })
-
       } else {
-        return <div></div>
+          let amenities = Object.entries(spaceAmenities).slice(0, 6);
+          return amenities.map((amenity, i) => {
+            if (amenity[0] === "hours_24_access") {
+              return (
+                <div className="amenities-item-mobile" key={i}>
+                  24/7 Access
+                </div>
+              );
+            } else if (amenity[0] === "phone_booths") {
+              return (
+                <div className="amenities-item-mobile" key={i}>
+                  {amenity[1]} phone booths
+                </div>
+              );
+            } else if (amenity[0] === "transit_station_miles") {
+              if (amenity[1] < 1) {
+                return (
+                  <div className="amenities-item-mobile" key={i}>
+                    {" "}
+                    &lt;1 mile to nearest transit station{" "}
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="amenities-item-mobile" key={i}>
+                    {" "}
+                    {amenity[1]} miles to nearest transit station{" "}
+                  </div>
+                );
+              }
+            } else if (amenity[0] === "nearest_parking") {
+              if (amenity[1] < 1) {
+                return (
+                  <div className="amenities-item-mobile" key={i}>
+                    &lt;1 mile to nearest parking lot
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="amenities-item-mobile" key={i}>
+                    {amenity[1]} miles to nearest parking lot{" "}
+                  </div>
+                );
+              }
+            } else if (amenity[0] === "meeting_rooms") {
+              return (
+                <div className="amenities-item-mobile" key={i}>
+                  {" "}
+                  {amenity[1]} meeting rooms{" "}
+                </div>
+              );
+            } else if (amenity[0] === "people_capacity") {
+              return (
+                <div className="amenities-item-mobile" key={i}>
+                  {" "}
+                  {amenity[1]} total capacity{" "}
+                </div>
+              );
+            } else {
+              return (
+                <div className="amenities-item-mobile" key={i}>
+                  {(amenity[0][0].toUpperCase() + amenity[0].slice(1))
+                    .split("_")
+                    .join(" ")}
+                </div>
+              );
+            }
+          });
       }
       
     } 
@@ -69,9 +133,29 @@ class SpaceShow extends React.Component {
     otherAmenities(spaceAmenities) {
       let numAmenities = Object.keys(spaceAmenities).length
       if (numAmenities > 6) {
-        return <div className="num-amenities-wrapper">
-                  <div className="num-amenities-text" onClick={() => this.props.openModal('amenities')}>Show all {numAmenities} amenities</div>
+        if (isBrowser) {
+          return (
+            <div className="num-amenities-wrapper">
+              <div
+                className="num-amenities-text"
+                onClick={() => this.props.openModal("amenities")}
+              >
+                Show all {numAmenities} amenities
+              </div>
+            </div>
+          );
+        } else {
+           return (
+             <div className="num-amenities-wrapper-mobile">
+               <div
+                 className="num-amenities-text-mobile"
+                 onClick={() => this.props.openModal("amenities")}
+               >
+                 Show all {numAmenities} amenities
                </div>
+             </div>
+           );
+        }
       }
     }
     
@@ -157,52 +241,48 @@ class SpaceShow extends React.Component {
             );
           } else {
             return (
-              <div className="space-show-main-div">
+              <div className="space-show-main-div-mobile">
                 <div className="space-pics-mobile">
                   <img
                     className="space-show-main-pic-mobile"
                     src={space.main_pic}
                   ></img>
                 </div>
-                <div className="space-info-wrapper">
-                  <div className="space-show-info">
-                    <div className="space-summary">
-                      <span className="space-show-name">{space.name}</span>
-                      <div className="space-show-city">{space.city}</div>
+                <div className="space-info-wrapper-mobile">
+                  <div className="space-show-info-mobile">
+                    <div>
+                      <div className="space-show-name-mobile">{space.name}</div>
+                      <div className="space-show-city-mobile">{space.city}</div>
                     </div>
                     <div className="description-wrapper">
-                      <p className="space-show-description">
+                      <p className="space-show-description-mobile">
                         {space.description}
                       </p>
                     </div>
-                    <div className="amenities-wrapper">
-                      <h2>Amenities</h2>
-                      <div className="amenities-div">
+                    <div className="amenities-wrapper-mobile">
+                      <div className="amenities-header-mobile">Amenities</div>
+                      <div className="amenities-div-mobile">
                         {this.includedAmenities(space.amenities)}
                       </div>
                       {this.otherAmenities(space.amenities)}
                     </div>
-                    <div className="reserve-widget-wrapper">
-                      <div className="reserve-widget">
-                        <span className="reserve-cost">${space.cost} </span>
-                        <span className="reserve-per-month">
-                          per desk / month
-                        </span>
+                    <div className="contact-info-wrapper-mobile">
+                      <div className="contact-header-mobile">Contact Information</div>
+                      <div className="contact-item-mobile">
+                        <a href={space.website}>{space.website}</a>
                       </div>
-                      <div className="contact-info-wrapper">
-                        <div className="contact-item-top">
-                          <a href={space.website}>{space.website}</a>
-                        </div>
-                        <div className="contact-item">{space.email}</div>
-                        <div className="contact-item">{space.phone}</div>
-                      </div>
+                      <div className="contact-item-mobile">{space.email}</div>
+                      <div className="contact-item-mobile">{space.phone}</div>
                     </div>
                   </div>
+                </div>
+                <div className="reserve-widget-mobile">
+                  <span className="reserve-cost-mobile">${space.cost} </span>
+                  <span className="reserve-per-month-mobile">per desk / month</span>
                 </div>
               </div>
             );
           }
-
         } else {
             return (
                 <div>
