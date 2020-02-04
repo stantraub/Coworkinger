4a5adeab2da70410eadfbb4c0c0974a2346247d1
Stanley <stanley.traub@gmail.com>
2020-02-03 16:24:54 -0800

    Removed old modal code

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,9 @@
 import { combineReducers } from 'redux';
 
 import entitiesReducer from "./entities_reducer";
-import ui_reducer from './ui_reducer';
+
 
 const RootReducer = combineReducers({
-    ui: ui_reducer,
     entities: entitiesReducer
 })
 
