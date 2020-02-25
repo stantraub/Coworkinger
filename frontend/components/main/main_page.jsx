ad669b5de8a77594159b82536d9fe92cc09f281d
Stanley <stanley.traub@gmail.com>
2020-02-24 16:31:54 -0800

    Added temp button to index page

--- frontend/components/main/main_page.jsx
+++ frontend/components/main/main_page.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import {
   BrowserView,
   MobileView,
@@ -13,9 +15,15 @@ class MainPage extends React.Component {
         <img src="https://s3.amazonaws.com/fj-employer-blog/employer-blog/wp-content/uploads/2016/12/30043608/The-Benefits-of-Coworking-Spaces-for-Corporations.jpg" className='main-page-background'></img>
         <div className="search-container">
           <h1 className='search-header'>Find a workspace near you</h1>
-          <form className='search-form-container'>
+          <Link to={"/spaces"}>
+            <button className="search-box">
+              All San Francisco Coworking Spaces
+            </button>
+          </Link>
+          {/*<form className='search-form-container'>
             <input className="search-box" placeholder="What type of workspace do you need?" type='text' />
           </form>
+         */}
         </div>
       </section>
 
